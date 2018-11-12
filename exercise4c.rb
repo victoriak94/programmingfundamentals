5c2f961e58d979fd5c35a5b391bd3cc4922b630f
victoriak94 <vkamila6@gmail.com>
2018-11-12 15:16:51 -0500

    Wrote an if else statement using a logical operator to return a message if user input is equal to the variable set to my name.

---
+++ exercise4c.rb
@@ -0,0 +1,11 @@
+my_name = "Victoria"
+
+puts "What is your name?"
+
+user_name = gets.chomp
+
+  if user_name == my_name
+    puts "We have the same name!"
+  else
+    puts "Oh. We don't have the same name :("
+end
