7e6e2a26bc3438daf662d3d4a055283b0de0b278
victoriak94 <vkamila6@gmail.com>
2018-11-12 13:53:43 -0500

    Explored strings, values, booleans, comparison and logical operators. Created new exercise 2 file.

--- exercise1.rb
+++ exercise1.rb
@@ -1,1 +1,6 @@
-puts 2 puts 3 puts 2 + 3
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
