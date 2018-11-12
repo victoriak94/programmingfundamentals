d03dd1540b78c21d61f8751f4143668f7379880f
victoriak94 <vkamila6@gmail.com>
2018-11-12 14:46:52 -0500

    Wrote ruby to acquire user input for user name and user age. Subtracted the user's age from the current year to determine the year they were born. Used chomp to get rid of the return in the message and converted user input to an integer.

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,8 @@
 puts "What is your name?"
+user_name = gets
+puts "Hello, #{user_name}"
+
+puts "How old are you?"
+user_age = gets.chomp.to_i
+this_year = 2018
+puts "You were born in #{this_year - user_age}"
