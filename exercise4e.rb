b0148549f91c1a742574240ecf40842a127b6304
victoriak94 <vkamila6@gmail.com>
2018-11-12 15:44:21 -0500

    Created new variable to calculate difference between secret number and input number. Created elsif statement to determine if difference between numbers is off by 1.

--- exercise4e.rb
+++ exercise4e.rb
@@ -3,10 +3,11 @@ secret_number = 4
 puts "What is your favourite number?"
 
 number = gets.chomp.to_i
+difference = (secret_number - number).abs
 
   if number == secret_number
     puts "You win!"
-  elsif number = (secret_number +-= 1)
+  elsif difference == 1
     puts "So close!"
   else
     puts "Try again"
