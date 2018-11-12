c01878442bc7eee5fe76742f4148cee914a4843b
victoriak94 <vkamila6@gmail.com>
2018-11-12 15:11:35 -0500

    Fixed order of if else statements to display message for if user age is greater than 105.

--- exercise4b.rb
+++ exercise4b.rb
@@ -2,10 +2,10 @@ puts "What is your age?"
 
 user_age = gets.chomp.to_i
 my_age = 24
-  if user_age >= my_age
-    puts "We are #{user_age - my_age} years apart in age!"
-  elsif user_age > 105
+  if user_age > 105
     puts "I'm not sure I believe you."
+  elsif user_age >= my_age
+    puts "We are #{user_age - my_age} years apart in age!"
   else
     puts "We are #{my_age - user_age} years apart in age!"
 end
